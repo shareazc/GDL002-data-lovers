0068e9e42d7c414853552313ebc52c63f152b794
Melisa Quesada <melisa.quc@gmail.com>
2019-02-20 12:20:23 -0600

    Función de búsqueda por nombre y número

--- src/main.js
+++ src/main.js
@@ -1,20 +1,75 @@
-let selectedPokemon = "Mew";
+let selectedPokemon;
+let pokemonObject;
 
-const pickPokemon = () => {
+const pickPokemon = () => { //Seleccionar un pokemon al azar
     const randomNumber = Math.floor(Math.random()*151);
-    randomPokemon = POKEMON.pokemon[randomNumber];
+    selectedPokemon = POKEMON.pokemon[randomNumber];
+    return selectedPokemon;
 };
 
-const getName = () => {
+const getPokemon = () => { //Recibir el nombre del pokemon a buscar
     selectedPokemon = document.getElementById("searchBar").value;
-    console.log(selectedPokemon);
     return selectedPokemon;
 }
 
-let searchPokemon = POKEMON.pokemon.find(pokemonName => pokemonName.name === selectedPokemon);
-console.table(searchPokemon);
+const findPokemonName = () => { //Encontrar pokemon por nombre
+    pokemonObject = POKEMON.pokemon.find(pokemonName => pokemonName.name === getPokemon());
+    console.table(pokemonObject);
+    return pokemonObject;
+}
 
-document.getElementById("searchButton").addEventListener("click",searchPokemon);
+const findPokemonNum = () => { //Encontrar pokemon por numero
+    pokemonObject = POKEMON.pokemon.find(pokemonId => pokemonId.id == getPokemon());
+    console.table(pokemonObject);
+    return pokemonObject;
+}
+
+const findPokemon = () => { //Realizar búsqueda
+    findPokemonName();
+    if(pokemonObject == undefined){
+        findPokemonNum();
+        showInfo();
+    } else {
+        showInfo();
+    }
+}
+
+let showInfo = () => { //Mostrar datos del pokemon
+    document.getElementById("pokeName").innerHTML = pokemonObject.name;
+    document.getElementById("pokeNum").innerHTML = pokemonObject.num;
+    document.getElementById("pokemonPicture").src = pokemonObject.img;
+    document.getElementById("pokeType").innerHTML = pokemonObject.type;
+    //document.getElementById("pokeEvolution").innerHTML = //leer todo el arreglo e imprimir (map? filter?)
+    document.getElementById("pokeDescription").innerHTML = pokemonObject.description + pokemonObject.height + pokemonObject.weight;
+    document.getElementById("pokeWeaknesses").innerHTML = pokemonObject.weaknesses;
+    document.getElementById("frequencySpawn").innerHTML = pokemonObject.avg_spawns;
+    document.getElementById("timeSpawn").innerHTML = pokemonObject.spawn_time;
+    document.getElementById("kmEgg").innerHTML = pokemonObject.egg;
+    document.getElementById("candies").innerHTML = pokemonObject.candy_count;
+
+}
+
+
+// EVENT LISTENERS
+document.getElementById("searchButton").addEventListener("click",findPokemon); //Llamar al botón BUSCAR
+
+
+
+/*const printPokemon = (itemToPrint) => {
+    document.getElementsByClassName("pokemonInfo").innerHTML = itemToPrint;
+}*/
+
+//const searchPokemon = POKEMON.pokemon.find(pokemonName => pokemonName.name === selectedPokemon); //Buscar un pokemon por nombre
+//console.table(searchPokemon);
+
+/* ESTA FUNCIONABA SOLO CON NOMBRE, AUN SIN NUM OR FAVOR NO LA CAGUES
+const findPokemon = () => {
+    const pokemonInfo = POKEMON.pokemon.find(pokemonName => pokemonName.name === getPokemonName());
+    console.table(pokemonInfo);
+    return pokemonInfo;
+}
+*/
+
 /* ESTA SI FUNCIONA
 const getName = () => {
     selectedPokemon = document.getElementById("searchBar").value;
